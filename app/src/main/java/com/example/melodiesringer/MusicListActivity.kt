35d387f7867ead8ee53b81cd59bdbbcbfe6b7cd8
MaksStalker1985 <89601993+MaksStalker1985@users.noreply.github.com>
2022-05-02 17:18:29 +0300

    2

--- app/src/main/java/com/example/melodiesringer/MusicListActivity.kt
+++ app/src/main/java/com/example/melodiesringer/MusicListActivity.kt
@@ -1,11 +1,17 @@
 package com.example.melodiesringer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MusicListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_music_list)
+        val closeButton = findViewById<ImageView>(R.id.imageView4)
+        closeButton.setOnClickListener {
+           finishAndRemoveTask()
+        }
+        }
     }
-}
--- app/src/main/java/com/example/melodiesringer/melodiesRingerService.kt
+++ app/src/main/java/com/example/melodiesringer/melodiesRingerService.kt
@@ -1,12 +1,28 @@
 package com.example.melodiesringer
 
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.IBinder
 
 class melodiesRingerService : Service() {
 
+    override fun onCreate() {
+        super.onCreate()
+
+
+
+    }
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
     }
 }
