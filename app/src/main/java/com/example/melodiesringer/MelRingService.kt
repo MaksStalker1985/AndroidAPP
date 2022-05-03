453139f608bca3d58d6ac2ed7e9f5d75eef75ccd
MaksStalker1985 <89601993+MaksStalker1985@users.noreply.github.com>
2022-05-03 14:05:23 +0300

    Update MelRingService.kt
    Update

--- app/src/main/java/com/example/melodiesringer/MelRingService.kt
+++ app/src/main/java/com/example/melodiesringer/MelRingService.kt
@@ -1,24 +1,37 @@
 package com.example.melodiesringer
 
+import android.app.Notification
 import android.app.Service
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import android.os.IBinder
 import android.telecom.Call
-import android.telecom.Call.Details
+import android.telephony.TelephonyManager
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 
 class MelRingService : Service() {
 
+    private val isRunning = false
+
     override fun onCreate() {
+
         super.onCreate()
     }
+
+
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
 
+        while (isRunning){
+
+        }
 
         return START_STICKY;
     }
